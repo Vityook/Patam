b061a4f17c4f68826d1f2e133bdcdde37a6fde7f
Vic <thevictator77@gmail.com>
2024-04-30 11:43:19 +0300

    Tile Update

--- Tile.java
+++ Tile.java
@@ -1,7 +1,34 @@
 package test;
 
 
-public class Tile {
+import java.io.ObjectInputStream;
+import java.util.Objects;
 
-	
+public class Tile {
+    public final char letter;
+    public final int score;
+
+    private Tile(int score, char letter) {
+        this.letter = letter;
+        this.score = score;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if(this == o) return true;
+        if(o == null || getClass() != o.getClass()) return false;
+        Tile tile = (Tile) o;
+        return score == tile.score && letter == tile.letter;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(score, letter);
+    }
+
+    public static class Bag{
+
+    }
+
+
 }
