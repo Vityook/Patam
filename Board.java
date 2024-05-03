9061d3e2d8440942fb34d926dd759711275fbdd6
Vic <thevictator77@gmail.com>
2024-05-03 21:00:28 +0300

    asdas

--- Board.java
+++ Board.java
@@ -2,5 +2,40 @@ package test;
 
 
 public class Board {
+    Tile[][] board;
 
+    static boolean middleCheck ;
+    int [] letterScore = {1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10}; //ascii start at 65=A -> 90=Z
+
+    public static char [][] boardScore =
+            {
+                    {'R','N','N','T','N','N','N','R','N','N','N','T','N','N','R'},
+                    {'N','Y','N','N','N','B','N','N','N','B','N','N','N','Y','N'},
+                    {'N','N','Y','N','N','N','T','N','T','N','N','N','Y','N','N'},
+                    {'T','N','N','Y','N','N','N','T','N','N','N','Y','N','N','T'},
+                    {'N','N','N','N','Y','N','N','N','N','N','Y','N','N','N','N'},
+                    {'N','B','N','N','N','B','N','N','N','B','N','N','N','B','N'},
+                    {'N','N','T','N','N','N','T','N','T','N','N','N','T','N','N'},
+                    {'R','N','N','T','N','N','N','C','N','N','N','T','N','N','R'},//8
+                    {'N','N','T','N','N','N','T','N','T','N','N','N','T','N','N'},
+                    {'N','B','N','N','N','B','N','N','N','B','N','N','N','B','N'},
+                    {'N','N','N','N','Y','N','N','N','N','N','Y','N','N','N','N'},
+                    {'T','N','N','Y','N','N','N','T','N','N','N','Y','N','N','T'},
+                    {'N','N','Y','N','N','N','T','N','T','N','N','N','Y','N','N'},
+                    {'N','Y','N','N','N','B','N','N','N','B','N','N','N','Y','N'},
+                    {'R','N','N','T','N','N','N','R','N','N','N','T','N','N','R'},
+            };
+    private Board() {
+        board = new Tile[15][15];
+        middleCheck = false;
+    }
+
+    private static Board instance = null;
+    public static Board getBoard(){
+        if(instance == null)
+            instance = new Board();
+        return instance;
+    }
+
+    
 }
--- Word.java
+++ Word.java
@@ -1,7 +1,47 @@
 package test;
 
 
-public class Word {
-	public Tile[] tiles ;
+import java.util.Arrays;
+import java.util.Objects;
 
+public class Word {
+	public Tile[] tiles;
+	public int row, col;
+	public boolean vertical;
+
+	public Word(Tile[] tiles, int row, int col, boolean vertical){
+		this.tiles = tiles;
+		this.row = row;
+		this.col = col;
+		this.vertical = vertical;
+	}
+
+	public int getCol() {
+		return col;
+	}
+
+	public Tile[] getTiles() {
+		return tiles;
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public boolean isVertical() {
+		return vertical;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Word word = (Word) o;
+		return row == word.row && col == word.col && vertical == word.vertical && Objects.deepEquals(tiles, word.tiles);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(Arrays.hashCode(tiles), row, col, vertical);
+	}
 }
